f2fc071dc260800dfe28f49a3551244873f35770
charnchol <khumball46@gmail.com>
2021-12-26 21:16:33 +0700

    lab 14-1 complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool a[][M],int b,int c,int d){
+    for(;;){
+        double h = pow((),2);
+        double i = pow((),2);
+        double j = sqrt((h+i));
+        if(j <= (b-1)) a[c][d] = 1 ;
+        else a[c][d] = 0 ;
+    }
+}
+
+void showImage(const bool e[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int f = 0; f < N; f++){
+        cout << '|';
+        for(int g = 0; g < M; g++){
+            if(e[f][g] == 0) cout << " ";
+            if(e[f][g] == 1) cout << "*";
+        }
+        cout << '|' << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
